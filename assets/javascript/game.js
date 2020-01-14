692ccc9cdde615b3ad2d9e37bba3ac629fdd3a5f
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-01-14 12:24:49 -0500

    html and javascript changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+
+// Variables to guess the word 
+var words = [Tiesto, Avicii, Eric Prydz, Armin Van Buuren, Above and Beyond, Carl Cox];
+
+// Variables for tracking our wins, losses and ties. They begin at 0.
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+
+// When the user presses a key, it will run the following function...
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+
+// Pick a random word
+ var word = words[Math.floor(Math.random() * words.length)];
+
+// Set up the answer array
+ var answerArray = [];
+
+ for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+ }
+ var remainingLetters = word.length;
+}
